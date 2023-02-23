5e4cb1fe3013f55749a10d7021605c8203dc8932
极客柒 <58359338+badApple001@users.noreply.github.com>
2023-02-24 01:11:55 +0800

    open

---
+++ Assets/ImageErasure/Scripts/ImageErasure.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageErasure : MonoBehaviour
+{
+    [SerializeField,Tooltip("是否需要适配全屏")]
+    private bool fitBgOnAwake = false;
+
+    private Image drawingBoard = null;
+
+    private void Awake()
+    {
+        drawingBoard = GetComponent<Image>();
+        if ( null == drawingBoard )
+        {
+            throw new UnityEngine.UnityException( "You need to drag it onto an object that contains the Image component" );
+        }
+
+        //full screen
+        if ( fitBgOnAwake )
+        {
+            RectTransform rectTransform = ( RectTransform ) transform;
+            RectTransform parentRectTransform = ( RectTransform ) transform.parent;
+            var size = rectTransform.sizeDelta;
+            rectTransform.localScale = Vector3.one * ( parentRectTransform.sizeDelta.y / size.y );
+        }
+    }
+
+
+}
---
+++ Assets/Main/EraseManager.cs
@@ -0,0 +1,70 @@
+﻿using ScratchCardAsset;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EraseManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    bool chan = false;
+    // Update is called once per frame
+    void Update()
+    {
+
+        if ( Input.GetKeyUp( KeyCode.Space ) && !chan )
+        {
+            chan = true;
+            LoadScratchCard( "Textures/Diaochan" );
+        }
+
+
+
+        if( Input.GetKeyUp( KeyCode.Q ) )
+        {
+            LoadScratchCard( "Textures/splash" );
+        }
+        else if(Input.GetKeyUp( KeyCode.W ) ) { 
+        
+            LoadScratchCard( "Textures/2" );
+        }
+    }
+
+    void LoadScratchCard( string img_resource_url )
+    {
+
+
+        var uiRoot = GameObject.Find( "Canvas/UI Form" ).GetComponent<RectTransform>();
+
+        var img = GenImageObj( uiRoot, img_resource_url );
+
+        var scratchCardManager = Instantiate( Resources.Load<GameObject>( "ScratchCard" ) ).GetComponent<ScratchCardManager>();
+
+        scratchCardManager.CanvasRendererCard = img;
+
+        img.raycastTarget = true; //启用 必要条件
+
+        img.transform.localScale = Vector3.one * 0.4f;
+
+        scratchCardManager.ScratchSurfaceSprite = img.sprite;
+    }
+
+
+    Image GenImageObj( Transform parent, string resource_url )
+    {
+        var obj = new GameObject();
+        obj.transform.SetParent( parent, false );
+        var img = obj.AddComponent<Image>();
+        img.sprite = Resources.Load<Sprite>( resource_url );
+        img.SetNativeSize();
+        obj.name = img.sprite.name;
+        return img;
+    }
+}
